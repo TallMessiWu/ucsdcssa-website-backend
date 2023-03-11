b85cf7e4e3ca31e3ee00a52cb05bfd1bab023cf9
(Messi) Junlin Wu <tallmessiwu@qq.com>
2023-03-11 04:17:43 -0800

    :memo: 完善了注释。

--- exts.py
+++ exts.py
@@ -2,6 +2,8 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_redis import FlaskRedis
 
+# exts.py中的exts是extensions的缩写
+
 db = SQLAlchemy()
 redis_captcha = FlaskRedis(config_prefix="CAPTCHA")
 redis_token = FlaskRedis(config_prefix="TOKEN")
